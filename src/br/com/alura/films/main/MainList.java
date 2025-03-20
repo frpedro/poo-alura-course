527d3ffc48b575e9c159282cc717216df1b265ab
Pedro <pedrofernandesctt@gmail.com>
2025-03-20 13:54:06 -0300

    trabalhando em arraylists

---
+++ src/br/com/alura/films/main/MainList.java
@@ -0,0 +1,41 @@
+package br.com.alura.films.main;
+
+import br.com.alura.films.model.Movie;
+import br.com.alura.films.model.Serie;
+import br.com.alura.films.model.Tittle;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class MainList {
+    public static void main(String[] args) {
+        // create objects
+        var scarface = new Movie("Scarface", 1973);
+        var smurf2 = new Movie("Smurf 2", 2005);
+        var euphoria = new Serie("Euphoria", 2019, 2);
+
+        // add the objects to the list
+        var catalog = new ArrayList<Tittle>();
+        catalog.add(scarface);
+        catalog.add(smurf2);
+        catalog.add(euphoria);
+
+        // object reference
+        var eph = euphoria;
+
+        // for ache for p
+        for (Tittle catalogPrint: catalog){
+            System.out.println(catalogPrint);}
+
+        // new list of artists
+        ArrayList<String> artists = new ArrayList<>();
+        artists.add("Adam Sandler");
+        artists.add("Patrick");
+        artists.add("Antony");
+
+        // sort year the catalog list
+        catalog.sort(Comparator.comparing(Tittle::getYear));
+        System.out.println("Ordenado por ano: " + catalog);
+    }
+}
